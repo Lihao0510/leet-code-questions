3b8873afde288305571b711bee978321ffdda3f8
Lihao <591353643@qq.com>
2020-11-25 01:25:13 +0800

    1. 调试SmartPromise, 初步实现异步功能;

--- js_basic/smart_promise.js
+++ js_basic/smart_promise.js
@@ -9,29 +9,56 @@ function SmartPromise(executor) {
     self.value = null;
     self.reason = null;
 
+    self.onFullfilledHandler = Function.prototype;
+    self.onRejectedHandler = Function.prototype;
+
     function resolve(value) {
-        console.log('Promise Resolve ==>', value);
-        self.value = value;
+        if (self.status === 'pending') {
+            self.value = value;
+            self.status = 'fullfilled';
+            self.onFullfilledHandler(self.value);
+        }
     }
 
     function reject(reason) {
-        console.log('Promise Reject ==>', value);
-        self.reason = reason;
+        if (self.status === 'pending') {
+            self.reason = reason;
+            self.status = 'rejected';
+            self.onRejectedHandler(new Error(self.reason))
+        }
     }  
 
     executor(resolve, reject);
 }
 
 SmartPromise.prototype.then = function (onfullfilled, onrejected) {
+    onfullfilled = typeof onfullfilled === 'function' ? onfullfilled: data => data;
+    onrejected = typeof onrejected === 'function' ? onrejected: err => {throw err};
 
+    console.log('当前实例 ==>', this);
+
+    if (this.status === 'fullfilled') {
+        setTimeout(() => {
+            onfullfilled(this.value);
+        }, 0)
+    }
+    if (this.status === 'rejected') {
+        setTimeout(() => {
+            onrejected(this.reason)
+        }, 0)
+    }
+    if (this.status === 'pending') {
+        this.onFullfilledHandler = onfullfilled;
+        this.onRejectedHandler = onrejected;
+    }
 }
 
 var promise = new SmartPromise(function(resolve, reject) {
-    resolve(true);
+    resolve('success!');
 })
 
-promise.then(function(result, err) {
+promise.then(function(result) {
     console.log('Promise Result ==>', result);
 })
 
-console.log('开始Promise调试 ==>', SmartPromise);
+console.log('开始Promise调试 ==>', promise);
