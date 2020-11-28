da30302cd1ed0b269259d497988743e29acc2643
Lihao <591353643@qq.com>
2020-11-28 18:03:30 +0800

    1. 提交继承测试;

--- playground.js
+++ playground.js
@@ -6,14 +6,25 @@ Student.prototype.say = function() {
     console.log('Say ==>', this.name);
 }
 
+function GoodStudent(name) {
+    Student.call(this, name);
+}
+
+
 Student.prototype.type = 'person';
 
 var student = new Student('Wangnima');
 
 student.say();
 
+GoodStudent.prototype = new Student();
+GoodStudent.prototype.constructor = GoodStudent;
+
+
 var student2 = new student.__proto__.constructor('Zhaorenzhi');
 
-console.log('StudentType ==>', student.name, student.type);
+var student3 = new GoodStudent('Gaoshuaijian');
+
 
 console.log(student, student2);
+console.log(student3);
